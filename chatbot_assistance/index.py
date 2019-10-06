60cf225250837291e5d1053a2c1ff59cc429db6b
itssekhar <itssekhar511@gmail.com>
2019-10-06 18:27:18 -0500

    updated API keys for the project

---
+++ chatbot_assistance/index.py
@@ -0,0 +1,34 @@
+# /index.py
+from flask import Flask, request, jsonify, render_template
+import os
+import dialogflow
+import requests
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/webhook', methods=['POST'])
+
+def webhook():
+    data = request.get_json(silent=True)
+    if data['queryResult']['queryText'] == 'yes':
+        reply = {
+            "fulfillmentText": "Ok. Tickets booked successfully.",
+        }
+        return jsonify(reply)
+
+    elif data['queryResult']['queryText'] == 'no':
+        reply = {
+            "fulfillmentText": "Ok. Booking cancelled.",
+        }
+        return jsonify(reply)
+
+# Run Flask app
+if __name__ == "__main__":
+    app.run()
+
