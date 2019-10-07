52407dad6fda156edf89251e9d5d3a08795309d0
itssekhar <itssekhar511@gmail.com>
2019-10-07 00:30:21 -0500

    bug fixed

--- chatbot_assistance/index.py
+++ chatbot_assistance/index.py
@@ -27,6 +27,26 @@ def webhook():
         }
         return jsonify(reply)
 
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    if text:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+        return response.query_result.fulfillment_text
+        
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    message = request.form['message']
+    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+    fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
+    response_text = { "message":  fulfillment_text }
+    return jsonify(response_text)
+
 # Run Flask app
 if __name__ == "__main__":
     app.run()
